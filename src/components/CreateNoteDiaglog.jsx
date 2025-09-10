f045313d096682c5eca3cce583c9361a0fa6c196
Tyson <tjjlee@ucdavis.edu>
2025-09-10 11:52:45 -0700

    feat(create-a-note): alert user if empty message

--- src/components/CreateNoteDiaglog.jsx
+++ src/components/CreateNoteDiaglog.jsx
@@ -15,12 +15,13 @@ import { useState } from 'react'
 
 function CreateNoteDialog () {
   const { setNotesState } = useNotes() // Get setNotesState from useContext(NotesContext)
-  const [content, setContent] = useState("") // Textarea's content
-  const [open, setOpen] = useState(false) // For tracking if the dialog is opened or closed
-  const [showAlert, setShowAlert] = useState(false) // For tracking if the no message alert should show or not
+  const [textareaContent, setTextareaContent] = useState("") // Textarea's content
+  const [dialogOpen, setDialogOpen] = useState(false) // For tracking if the dialog is opened or closed
+  const [showEmptyAlert, setShowEmptyAlert] = useState(false) // For tracking if the no message alert should show or not
 
-  function renderAlert() {
-    if (!showAlert) return null
+  function renderEmptyAlert() {
+    console.log(showEmptyAlert)
+    if (!showEmptyAlert) return null
     else return (
       <Alert variant="destructive">
         <AlertTitle className="font-bold">Warning</AlertTitle>
@@ -32,17 +33,18 @@ function CreateNoteDialog () {
   }
 
   function handleSaveNote() {
-    if (!content.trim()) {
-      setShowAlert(true)
+    // Show empty alert if textarea is empty
+    if (!textareaContent.trim()) {
+      setShowEmptyAlert(true)
       return 
-    } // TODO: Implement alert
+    } 
 
     // Create new note information
     const newId = crypto.randomUUID()
     const now = Date.now()
     const newNote = {
       id: newId,
-      content: content,
+      content: textareaContent,
       createdAt: now,
       wasUpdated: false,   
       updatedAt: null, 
@@ -59,22 +61,29 @@ function CreateNoteDialog () {
       byOrderActive: [newId, ...prev.byOrderActive] 
     }))
     
-    // Reset content & close the dialog
-    setContent("") 
-    setOpen(false)
+    // Reset states
+    setTextareaContent("") 
+    setDialogOpen(false)
+    setShowEmptyAlert(false)
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={dialogOpen} onOpenChange={(isOpen) => {
+      setDialogOpen(isOpen)
+      if (!isOpen) setShowEmptyAlert(false)
+    }}>
       <DialogTrigger className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-slate-950 px-4 py-2 text-sm font-medium text-foreground transition-colors hover:bg-accent focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-accent-foreground data-[state=open]:bg-accent/50 data-[state=open]:hover:bg-accent data-[state=open]:focus:bg-accent">
         <IoMdCreate className="w-5 h-5 pr-1"/> Create a Note
       </DialogTrigger>
       <DialogContent className="flex items-center justify-center max-w-[80%] max-h-[60%] w-full h-full">
         <DialogHeader>
           <DialogTitle>Create a Note</DialogTitle>
-          {renderAlert()}
-          <DialogDescription className="flex flex-col">
-            <Textarea placeholder="Write your note here..." value={content} onChange={(event) => setContent(event.target.value)} className="flex max-w-[100%] max-h-[100%] w-full h-full resize-none text-foreground"/>
+          {renderEmptyAlert()}
+          <DialogDescription className="grid w-full gap-2">
+            <Textarea placeholder="Write your note here..." value={textareaContent} onChange={(event) => {
+              setTextareaContent(event.target.value)
+              if (showEmptyAlert) setShowEmptyAlert(false)
+            }} className="flex max-w-[100%] max-h-[100%] w-full h-full resize-none text-foreground"/>
             <Button variant="outline" onClick={handleSaveNote} className="bg-foreground text-background">Save Note</Button>
           </DialogDescription>
         </DialogHeader>
