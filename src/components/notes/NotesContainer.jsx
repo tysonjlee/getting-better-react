daa7ad3f96753507ad37aa78682aba4f3b8f0f80
Tyson <tjjlee@ucdavis.edu>
2025-09-10 17:41:39 -0700

    feat: implement PropTypes for dynamic NotesContainer

--- src/components/notes/NotesContainer.jsx
+++ src/components/notes/NotesContainer.jsx
@@ -1,17 +1,24 @@
 import NoteCard from "@/components/notes/NoteCard"
-import { useNotes } from "@/contexts/NotesContext"
+import PropTypes from 'prop-types'
 
-function NotesContainer({ numNotes }) {
-  const { notesState } = useNotes()
-	const notesToDisplay = notesState.byOrderActive.slice(0, numNotes)
+function NotesContainer({ noteArrays, limit }) {
+	const uniqueIds = [...new Set(noteArrays.flat())].slice(0, limit)
 
   return (
 		<div className="flex flex-row justify-center content-center items-center flex-wrap max-w-[90%] max-h-[90%] w-full h-full py-5 gap-5"> {/* Notes Container */}
-			{notesToDisplay.map((id) => (
+			{uniqueIds.map((id) => (
 				<NoteCard key={id} id={id}/>
 			))}
 		</div>
 	)
+	
+}
+
+NotesContainer.propTypes = {
+  limit: PropTypes.number.isRequired,
+  noteArrays: PropTypes.arrayOf(
+    PropTypes.arrayOf(PropTypes.string) 
+  ).isRequired
 }
 
 export default NotesContainer
