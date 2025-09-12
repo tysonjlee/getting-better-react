22acb7918c32519aa1b65d519eb16253285ff776
Tyson <tjjlee@ucdavis.edu>
2025-09-12 15:52:36 -0700

    feat(note-card-tags): show tags & add a tag button (non-functional) in dialog

--- src/components/notes/note-card/note-card-dialog/NoteCardDialogTags.jsx
+++ src/components/notes/note-card/note-card-dialog/NoteCardDialogTags.jsx
@@ -1,6 +1,24 @@
-function NoteCardDialogTags() {
+import { useNotes } from "@/contexts/NotesContext"
+import Tag from "@/components/notes/note-card/note-card-ui/Tag"
+import { LuCircleFadingPlus } from "react-icons/lu";
+
+function NoteCardDialogTags({ id }) {
+  const { notesState } = useNotes()
+  const note = notesState.byId[id]
+  const tags = note.tags
+
   return (
-    <></>
+    <div className="flex flex-row items-center flex-wrap gap-3">
+      {/* Individual Tags */}
+      {tags.map((tag) => (
+        <Tag key={tag} tag={tag}/>
+      ))}      
+
+      {/* Add Tag */}
+      <button>
+        <LuCircleFadingPlus size={18}/>
+      </button>
+    </div>
   )
 }
 
