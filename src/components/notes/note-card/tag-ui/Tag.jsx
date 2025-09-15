85e04c8b103f9c5e7ee6b883e31fa4a8bfab0de2
Tyson <tjjlee@ucdavis.edu>
2025-09-15 14:03:16 -0700

    wip(note-card-tags): make tag button action dynamic
    (same logic as icon determination)

--- src/components/notes/note-card/tag-ui/Tag.jsx
+++ src/components/notes/note-card/tag-ui/Tag.jsx
@@ -3,24 +3,30 @@ import { XIcon } from "lucide-react"
 import { FiPlus } from "react-icons/fi";
 
 function Tag({ tag, icon }) {
-  function handleTagAdd() {
-    // TODO: Implement
-  }
-
-  function handleTagDelete() {
-    // TODO: Implement
-  }    
-
-
   function determineIcon() {
     if (icon === "delete") return <XIcon size={15} />
     else if (icon === "add") return <FiPlus size={15}/>
   }
 
+  function determineTagAction() {
+    if (icon === "delete") return handleTagDelete
+    else if (icon === "add") return handleTagAdd
+  }
+
+  function handleTagAdd() {
+    /** @note For the Tag Add Menu */
+
+    
+  }
+
+  function handleTagDelete() {
+    /** @note For the note dialog tags */
+  }    
+
   return (
     <Badge variant="outline" className="relative min-w-20 min-h-5 group pr-2 rounded-full hover:pr-6 transition-all">
       <span>{tag}</span>
-      <button onClick={handleTagDelete} className="absolute right-1 opacity-0 group-hover:opacity-100 text-xs transition-opacity">
+      <button onClick={determineTagAction()} className="absolute right-1 opacity-0 group-hover:opacity-100 text-xs transition-opacity">
         {determineIcon()}
       </button>
     </Badge>
