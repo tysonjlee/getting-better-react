b81c1dd73021ab79b71ada6f2dabb3d358687f1f
Tyson <tjjlee@ucdavis.edu>
2025-09-16 16:37:18 -0700

    feat(tag-add-menu): display only unapplied tags

--- src/components/notes/note-card/tag-ui/TagAddMenu.jsx
+++ src/components/notes/note-card/tag-ui/TagAddMenu.jsx
@@ -11,16 +11,19 @@ import {
 } from "@/components/ui/Command"
 import { tagNames } from "@/contexts/NotesContext"
 import Tag from "@/components/notes/note-card/tag-ui/Tag"
+import { useNotes } from "@/contexts/NotesContext"
 
 function TagAddMenu({ id }) {
-
+  const { notesState } = useNotes()
+  const noteTags = notesState.byId[id].tags 
+  const uniqueTagNames = tagNames.filter((tag) => !noteTags.includes(tag))
 
   return (
     <Command className="bg-slate-950">
       <CommandInput placeholder="Search for a tag..." className="text-foreground"/>
       <CommandList>
         <CommandGroup heading="Tags" className="px-2 pb-44">
-          {tagNames.map((tag) => (
+          {uniqueTagNames.map((tag) => (
             <Tag key={tag} tag={tag} mode="add" id={id}/>
           ))}
         </CommandGroup>
