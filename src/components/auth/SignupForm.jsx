42053e4fb053d3f16fe120b6c44fb1229c1cdaef
Tyson <tjjlee@ucdavis.edu>
2025-09-23 15:13:54 -0700

    chore: remove forgot password link from signup form

--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -60,12 +60,6 @@ function SignupForm({ setShowLogin, setShowSignup }) {
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <a
-                    
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
                 </div>
                 <Input 
                   id="password" 
