62a22003a06b6f942183db70874c2724e9de699c
Tyson <tjjlee@ucdavis.edu>
2025-10-18 14:45:29 -0700

    feat: add gaps between landing page sections

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "./HeroSection"
+import MissionSection from "./MissionSection"
 
 function Landing() {
   return (
-    <div className="flex flex-col justify-center items-center py-2">
+    <div className="flex flex-col justify-center items-center py-4 gap-5">
       <HeroSection />
+      <MissionSection />
     </div>
   )
 }
