878510432167e9d36feac151b1494b34fd414e6c
Tyson <tjjlee@ucdavis.edu>
2025-10-18 14:32:19 -0700

    feat: implement proper landing page, auth, etc. routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import './App.css'
-import Navbar from '@/components/layout/Navbar'
 import AppRoutes from './routes'
-import { useSession } from '@supabase/auth-helpers-react'
-import { Landing } from './pages/Landing'
 
 function App() {
-	// Get user's current session
-	const session = useSession()
-
-	// If no session (user not logged in), show the User Authentication page
-	if (!session) return <Landing />
-	// Otherwise show the app
-	else
-		return (
-			<>
-				<Navbar />
-				<AppRoutes />
-			</>
-		)
+		return <AppRoutes />
 }
 
 export default App
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,34 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
 import { Landing } from '@/pages/Landing'
+import { UserAuthentication } from './pages/UserAuthentication'
 import { Home } from '@/pages/Home'
 import { Manager } from '@/pages/Manager'
+import Navbar from '@/components/layout/Navbar'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useSession } from '@supabase/auth-helpers-react'
 
 function AppRoutes() {
+	const session = useSession()
+	const isLoggedIn = !!session // '!!' forces a boolean value 
+
 	return (
-		<Routes>
-			<Route path="/" element={<Landing />} />
-			<Route path="/home" element={<Home />} />
-			<Route path="/manager" element={<Manager />} />
-		</Routes>
+		<>
+			{/* Show Navbar only for logged-in users */}
+			{isLoggedIn && <Navbar />}
+
+			{/* App Routes */}
+			<Routes>
+				{/* 404 Handling (user can type any invalid url and it'll redirect to this) */}
+				<Route path="*" element={isLoggedIn ? <Home /> : <Navigate to="/" />} />
+
+				{/* Public Routes (for anyone) */}
+				<Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <Landing />} />
+				<Route path="/auth" element={isLoggedIn ? <Navigate to="/home" /> : <UserAuthentication />} />
+
+				{/* Protected Routes (only for logged-in users) */}
+				<Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/" />} />
+				<Route path="/manager" element={isLoggedIn ? <Manager /> : <Navigate to="/" />} />
+			</Routes>
+		</>
 	)
 }
 
